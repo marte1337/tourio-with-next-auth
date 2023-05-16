6fea440dc181960c06a93dab4830e2c0d72bfaeb
Roland-Hufnagel <roland.hufnagel@gmx.de>
2023-05-16 15:54:29 +0200

    finish next-auth

--- pages/create.js
+++ pages/create.js
@@ -3,13 +3,14 @@ import styled from "styled-components";
 import { useRouter } from "next/router";
 import Form from "../components/Form.js";
 import { StyledLink } from "../components/StyledLink.js";
-
+import { useSession } from "next-auth/react";
 const StyledBackLink = styled(StyledLink)`
   justify-self: flex-start;
 `;
 
 export default function CreatePlacePage() {
   const router = useRouter();
+  const { data: session, status } = useSession();
 
   async function addPlace(place) {
     const response = await fetch("/api/places", {
@@ -26,7 +27,7 @@ export default function CreatePlacePage() {
       console.error(`Error: ${response.status}`);
     }
   }
-
+if (status !== 'authenticated'){return <h1>Access denied</h1>}
   return (
     <>
       <h2 id="add-place">Add Place</h2>
